7249ee3754838e3a2be5ca7260b467f44171460e
Justin Nichols <ninetyonenichols@gmail.com>
2020-06-26 14:17:14 -0700

    Added placeholder Javascript

---
+++ public_html/js/jumble.js
@@ -0,0 +1,48 @@
+/*
+ * Author: Justin Nichols
+ * Class: CSC337 (Web Development)
+ * Purpose: This page adds functionality to index.html.
+ *          Specifically, it allows the user to update the ciphers / cipher-texts.
+ */
+
+/*
+ * Updates the label for the Caesar-cipher's key
+ */
+function setCaesarKeyLabel() {
+}
+
+/*
+ * Updates the square for the Square-cipher's key
+ */
+function setSquareKey() {
+}
+
+/*
+ * Updates the Caesar cipher-text
+ */
+function setCaesarText() {
+}
+
+/*
+ * Updates the Square cipher-text
+ */
+function setSquareText() {
+}
+
+/*
+ * Updates both cipher-texts
+ */
+function setCipherTexts() {
+    setCaesarText();
+    setSquareText();
+}
+
+/*
+ * Initializes the page upon loading
+ */
+function initPage() {
+    setCaesarKeyLabel();
+    setSquareKey();
+    setCipherTexts();
+}
+
